b71844b17b5f0eed043dcd16077eb268013af291
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-14 20:13:02 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,8 @@
+numero = float(input('Me diga um numero: '))
+numero2 = float(input('Me diga outro numero: '))
+if numero > numero2:
+    print(f'O numero {numero} é Maior que o numero {numero2}')
+elif numero == numero2:
+    print()
+else:
+    print(f'O numero {numero2} é Maior que o numero {numero}')
