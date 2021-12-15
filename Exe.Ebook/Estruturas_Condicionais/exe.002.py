2a642e672efc5f08d15b2037b27f91e449d83812
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-15 17:24:47 -0300

    Create exe.002.py

---
+++ Exe.Ebook/Estruturas_Condicionais/exe.002.py
@@ -0,0 +1,7 @@
+'''Programa Python para verificar se um número é par.'''
+
+numero = float(input('Me diga um numero: '))
+if (numero % 2) == 0:
+    print(f'O Numero {numero} é Par!')
+else:
+    print(f'O Numero {numero} impar!')
