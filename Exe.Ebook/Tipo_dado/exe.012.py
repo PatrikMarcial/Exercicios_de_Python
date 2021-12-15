5cb2d2f8bf18bd9436893d3e82fa5474867257fc
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-15 17:25:06 -0300

    Delete exe.012.py

--- Exe.Ebook/Tipo_dado/exe.012.py
+++
@@ -1,7 +0,0 @@
-'''Programa Python para verificar se um número é par.'''
-
-numero = float(input('Me diga um numero: '))
-if (numero % 2) == 0:
-    print(f'O Numero {numero} é Par!')
-else:
-    print(f'O Numero {numero} impar!')
