ced8bc26054919e36e12452308f5e233ce780686
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-18 20:13:50 -0300

    exercicio tipos

---
+++ Exe.Ebook/Estruturas_de_repetição/exe.002.py
@@ -0,0 +1,6 @@
+'''Escreva um programa que imprima todos os números inteiros do intervalo
+fechado de 1 a 100.'''
+
+for a in range(1, 101):
+    print(a)
+
---
+++ Exe.Ebook/Estruturas_de_repetição/exe.003.py
@@ -0,0 +1,8 @@
+'''Escreva um programa que imprima todos os números inteiros de 100 a 1 (em
+ordem decrescente).'''
+
+for a in range(100, 0, -1):
+    print(a)
+
+
+
