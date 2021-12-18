24ff8b10bd76923428f51b13a22aa1a7040229d5
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-18 20:19:28 -0300

    exercicio tipos

---
+++ Exe.Ebook/Estruturas_de_repetição/exe.004.py
@@ -0,0 +1,4 @@
+'''Escreva um programa que imprima todos os números inteiros de 100 a 200'''
+
+for a in range(100, 201):
+    print(a)
---
+++ Exe.Ebook/Estruturas_de_repetição/exe.005.py
@@ -0,0 +1,6 @@
+'''Escreva um programa que imprima todos os números inteiros de 200 a 100
+(em ordem decrescente).'''
+
+for a in range(200, 99, -1):
+    print(a)
+
