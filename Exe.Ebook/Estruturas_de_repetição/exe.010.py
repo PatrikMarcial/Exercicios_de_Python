b2b6788ee063e98305e05d04d5b4321f53a19c30
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-18 21:09:44 -0300

    exercicio tipos

---
+++ Exe.Ebook/Estruturas_de_repetição/exe.010.py
@@ -0,0 +1,7 @@
+'''Escreva um programa que receba 5 números do usuário e imprima a metade
+de cada número.'''
+
+for a in range(1, 6):
+    numero = int(input('Digite um numero: '))
+    b = numero / 2
+    print(b)
