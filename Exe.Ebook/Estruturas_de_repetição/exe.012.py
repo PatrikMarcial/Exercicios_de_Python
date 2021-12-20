3a78d585918aa74805a455b65b45bc948bda4bcc
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-20 18:12:03 -0300

    exercicio tipos

---
+++ Exe.Ebook/Estruturas_de_repetição/exe.012.py
@@ -0,0 +1,12 @@
+'''Criar um programa que imprima todos os números de 1 até 100, inclusive, e a
+soma de todos eles.'''
+
+soma = 0
+for a in range(1, 100+1):
+    soma += a
+    print(a)
+print(f'A soma de todos esses numeros é {soma}')
+
+
+
+
