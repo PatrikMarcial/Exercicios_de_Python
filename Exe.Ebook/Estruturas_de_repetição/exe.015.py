a8451cfdcc702e36d78e1095f565c4b332ee201e
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-20 18:57:22 -0300

    exercicio tipos

---
+++ Exe.Ebook/Estruturas_de_repetição/exe.015.py
@@ -0,0 +1,16 @@
+'''Criar um programa que leia dez números inteiros e imprima o maior e o menor
+número da lista'''
+
+quant = maior = menor = 0
+for a in range(1, 11):
+    num = int(input('Me diga um numero: '))
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+print(f'O maior e menor valor desses numeros digitados foram {maior} e {menor}')
+
