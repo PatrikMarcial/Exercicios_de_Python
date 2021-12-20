c2d184463341a8876ed36623b4d029c5fb8f24ed
Petrik Marcial <patrikmarcial2@gmail.com>
2021-12-20 18:01:11 -0300

    exercicio tipos

---
+++ Exe.Ebook/Estruturas_de_repetição/exe.011.py
@@ -0,0 +1,8 @@
+'''.Escreva um programa que receba 5 números do usuário e imprima o quadrado
+de cada número.'''
+
+for a in range(1, 6):
+    numero = int(input('Digite um numero: '))
+    b = numero ** 2
+    print(b)
+
